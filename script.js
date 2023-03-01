6206637c24083fa0121d24262e3882eaf68aeb96
Alumno <guillermo.menrem@gmail.com>
2023-03-01 14:08:07 +0100

    Añadido Escape

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+// Declaración de variables:
 const modal = document.querySelector(".modal");
-const btnsShowModal = document.querySelectorAll(".show-modal");
+const btnsShowModal = document.querySelectorAll("#botones");
 const btnCloseModal = document.querySelector(".close-modal");
 const overlay = document.querySelector(".overlay");
 
 // Visualización de elementos en consola:
-console.log(modal, btnsShowModal, btnCloseModal, overlay);
+// console.log(modal, btnsShowModal, btnCloseModal, overlay);
 
-// Funciones para mostrar y ocultar modales:
-const openModal = function () {
+// Funciones para mostrar y ocultar modales (quitado o añadiendo la clase hidden):
+const openModal = function (e) {
+  console.log(e, e.target);
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
@@ -17,7 +19,15 @@ const closeModal = function () {
   overlay.classList.add("hidden");
 };
 
-// Eventos de modales:
+// Eventos de modales aplicados a las variables:
 btnsShowModal.forEach((btn) => btn.addEventListener("click", openModal));
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+// Cerrar modales mediante la tecla Escape:
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+  e.stopPropagation();
+});
